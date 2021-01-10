541cd4ee4022d8790bf2e70b16ef18e6bd722c5f
Mehmet Seven <mehmetseven0@gmail.com>
2021-01-10 13:33:37 +0300

    express port

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/users", (req, res) => {
-	res.json([{ name: "Mehmet" }, { name: "Ahmet" }]);
+app.get('/users', (req, res) => {
+	res.json([{ name: 'Mehmet' }, { name: 'Ahmet' }]);
 });
 
-app.post("/add-user", (req, res) => {
+app.post('/add-user', (req, res) => {
 	res.json(req.body);
 });
 
-app.listen(4000, () => console.log("Server is up!"));
+app.listen(3000, () => console.log('Server is up!'));
